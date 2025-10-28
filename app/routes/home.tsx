e781c14c64841ee5a744fce93fbf519cc266e1cd
jimin <jiminseo0307@gmail.com>
2025-10-28 23:13:20 +0900

    feat: 1.5 install shadcn cli

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,10 @@
+import { Button } from "~/components/ui/button";
 import type { Route } from "./+types/home";
 
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+  <h1>
+    home <Button>Click Me</Button>
+  </h1>
+  );
 }
